647df4404b1e4c0bb0d236abd82a75d68f68e32d
matheb <mathebogi@gmail.com>
2017-01-11 12:09:48 +0100

    input validation restructured

--- input_validation.js
+++ input_validation.js
@@ -1,26 +1,23 @@
 var lista = ["amazing", "awesome", "blithesome", "excellent", "fabulous", "fantastic", "favorable", "fortuitous", "great", "incredible", "ineffable", "mirthful", "outstanding", "perfect", "propitious", "remarkable", "smart", "spectacular", "splendid", "stellar", "stupendous", "super", "ultimate", "unbelievable", "wondrous"];
 
-var feedback = "stellar stupendous super";
-var email = "alma@valami.hu"
-var scale = 4;
-
 function validator(feedback, scale, email){
   var checkMail = email.includes("@");
   var feedbackWords = feedback.split(' ');
   var counter = 0;
-  var countNiceWords = function(feedbackWords, lista){
-    for ( var i = 0; i < feedbackWords.length; i++){
-      if ( lista.includes(feedbackWords[i])){
-        counter++;
-      };
-    };
-  };
-  countNiceWords(feedbackWords, lista);
-  if(checkMail && scale >= 10 && counter >= 3){
+  if(checkMail && scale >= 10 && countNiceWords(feedbackWords, lista, counter) >= 3){
     return true;
   } else {
     return false;
   };
+};
+
+var countNiceWords = function(feedbackWords, lista, counter){
+  for ( var i = 0; i < feedbackWords.length; i++){
+    if ( lista.includes(feedbackWords[i])){
+      counter++;
+    };
+  };
+  return counter;
 };
 
 module.exports = validator;
