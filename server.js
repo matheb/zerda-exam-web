9b0a7dd5ba958cc4a8bb85f5a763b1d1f53e00a7
matheb <mathebogi@gmail.com>
2017-01-11 09:50:20 +0100

    Server serves static files

--- server.js
+++ server.js
@@ -10,17 +10,24 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/', express.static('./public'));
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password:'P8r8gl1d1ng',
-  database: 'app',
-});
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password:'P8r8gl1d1ng',
+//   database: 'app',
+// });
+//
+// con.connect(function(err){
+//   if(err){
+//     console.log("JAAAAJ");
+//   } else {
+//   console.log("SIKERULT");
+//   };
+// });
 
-con.connect(function(err){
-  if(err){
-    console.log("JAAAAJ");
-  } else {
-  console.log("SIKERULT");
-  };
-});
+app.get('/', function(req, res) {
+   res.sendfile('./public/index.html');
+ });
+
+
+app.listen(3000);
