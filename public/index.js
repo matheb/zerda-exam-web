f2fb26a837fb5f0a6254a6987363759823687e5a
matheb <mathebogi@gmail.com>
2017-01-11 09:46:10 +0100

    frontend almost ok

--- public/index.js
+++ public/index.js
@@ -1,1 +1,26 @@
 var sendButton = document.querySelector('button');
+
+sendButton.addEventListener('click', function(){
+  console.log('LOADING');
+})
+
+function postAnswer() {
+  var textValue =  document.querySelector('textarea').value;
+  var scaleValue =  document.querySelector('scale').value;
+  var emailValue =  document.querySelector('email').value;
+  if ( textValue != '' && scaleValue !='' && emailValue !=''){
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', "http://localhost:3000/exam", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({'feedback': textValue, 'scale': scaleValue, 'email': emailValue}));
+    xhr.onreadystatechange = function (){
+      if (xhr.readyState === XMLHttpRequest.DONE){
+        decoded.textContent = JSON.parse(xhr.response)['text'];
+      } else if (xhr.readyState === XMLHttpRequest.LOADING) {
+        setInterval(console.log('LOADING'), 1000);
+      };
+    };
+  } else {
+    alert('Please add an iput!');
+  };
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+var mysql = require("mysql");
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/', express.static('./public'));
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password:'P8r8gl1d1ng',
+  database: 'app',
+});
+
+con.connect(function(err){
+  if(err){
+    console.log("JAAAAJ");
+  } else {
+  console.log("SIKERULT");
+  };
+});
