88392b94a5cd7f4e2f14768d05ba805082dacd71
matheb <mathebogi@gmail.com>
2017-01-11 11:34:38 +0100

    All connections working, listing response, display Loading needed

--- public/index.js
+++ public/index.js
@@ -1,26 +1,34 @@
 var sendButton = document.querySelector('button');
+var result = document.querySelector('.list');
+
 
 sendButton.addEventListener('click', function(){
   console.log('LOADING');
+  postAnswer();
 })
 
 function postAnswer() {
   var textValue =  document.querySelector('textarea').value;
-  var scaleValue =  document.querySelector('scale').value;
-  var emailValue =  document.querySelector('email').value;
-  if ( textValue != '' && scaleValue !='' && emailValue !=''){
+  var scaleValue =  document.querySelector('#scale').value;
+  var emailValue =  document.querySelector('#email').value;
+  if ( textValue != '' && scaleValue != '' && emailValue != ''){
     var xhr = new XMLHttpRequest();
     xhr.open('POST', "http://localhost:3000/exam", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify({'feedback': textValue, 'scale': scaleValue, 'email': emailValue}));
     xhr.onreadystatechange = function (){
       if (xhr.readyState === XMLHttpRequest.DONE){
-        decoded.textContent = JSON.parse(xhr.response)['text'];
+        console.log(xhr.response);
+        if (xhr.response.includes("projects")){
+          result.textContent = JSON.parse(xhr.response)['projects'];
+        } else {
+          result.textContent = JSON.parse(xhr.response)['text'];
+        }
       } else if (xhr.readyState === XMLHttpRequest.LOADING) {
         setInterval(console.log('LOADING'), 1000);
       };
     };
   } else {
-    alert('Please add an iput!');
+    alert('Please add valid input!');
   };
 };
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var mysql = require("mysql");
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var validation = require('./input_validation.js');
 
 var app = express();
 
@@ -10,37 +11,43 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/', express.static('./public'));
 
-// var con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password:'P8r8gl1d1ng',
-//   database: 'app',
-// });
-//
-// con.connect(function(err){
-//   if(err){
-//     console.log("JAAAAJ");
-//   } else {
-//   console.log("SIKERULT");
-//   };
-// });
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password:'P8r8gl1d1ng',
+  database: 'secretprojects',
+});
+
+con.connect(function(err){
+  if(err){
+    console.log("JAAAAJ");
+  } else {
+  console.log("SIKERULT");
+  };
+});
 
 app.get('/', function(req, res) {
    res.sendfile('./public/index.html');
  });
 
- decoder.post('/exam', function add(req, resp) {
-  if (req.body.feedback !== '' && req.body.scale !== '' && req.body.email !== ''){
-    if ( req.body.shift > -1 && req.body.shift < 11 ) {
-      con.query('INSERT INTO texts SET ?', [{decoded: decode(req.body.text, req.body.shift)}], function(err,res){
-        resp.status(200).send({status: 'ok', projects: decode(req.body.text, req.body.shift)});
-      });
-    } else {
-      resp.status(400).send({status: 'error', text: 'thank you'});
-    };
+ app.post('/exam', function add(req, resp) {
+  if (validation(req.body.feedback, req.body.scale, req.body.email)){
+    con.query('SELECT project_name FROM projects', function(err,rows){
+      console.log(rows);
+      if(err) {
+        console.log(err.toString());
+        return;
+      } else {
+        var projects = [];
+        for (var i = 0; i < rows.length; i++){
+          projects.push(rows[i].project_name);
+        }
+        resp.status(200).send({status: 'ok', projects: projects});
+      }
+    });
   } else {
     resp.status(400).send({status: 'error', text: 'thank you'});
-  }
+  };
 });
 
 
