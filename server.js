856a44e2e1ca8dcbd6a94ee33e4da3d193249a9c
matheb <mathebogi@gmail.com>
2017-01-11 09:55:46 +0100

    backend node,express almost ok

--- server.js
+++ server.js
@@ -29,5 +29,19 @@ app.get('/', function(req, res) {
    res.sendfile('./public/index.html');
  });
 
+ decoder.post('/exam', function add(req, resp) {
+  if (req.body.feedback !== '' && req.body.scale !== '' && req.body.email !== ''){
+    if ( req.body.shift > -1 && req.body.shift < 11 ) {
+      con.query('INSERT INTO texts SET ?', [{decoded: decode(req.body.text, req.body.shift)}], function(err,res){
+        resp.status(200).send({status: 'ok', projects: decode(req.body.text, req.body.shift)});
+      });
+    } else {
+      resp.status(400).send({status: 'error', text: 'thank you'});
+    };
+  } else {
+    resp.status(400).send({status: 'error', text: 'thank you'});
+  }
+});
+
 
 app.listen(3000);
